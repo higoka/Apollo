ab825bcab842aaa838fd4ad46a62b10edcca3743
Cosimo Celeste <gogliacosimoceleste@gmail.com>
2021-08-22 20:29:21 +0200

    Update PluginTest.executor.ts

--- plugins/PluginTest/PluginTest.executor.ts
+++ plugins/PluginTest/PluginTest.executor.ts
@@ -13,5 +13,8 @@ export class PluginTestExecutor {
         this.pluginManager.readEvent("user.logged.in", (event: UserLoggedInEvent) => {
             this.logger.log(event.habbo.getHabboData.getUsername + " is logged to client");
         })
+        this.pluginManager.readEvent("apollo.shutdown.started", () => {
+            this.logger.warn("Apollo in shutdown! Plugin stopped!");
+        })
     }
 }
