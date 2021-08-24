1897d676929b748b3f81f7bbf931a99862ecfbb3
Cosimo Celeste <gogliacosimoceleste@gmail.com>
2021-08-24 10:53:16 +0200

    Update ip whitelist for RCON

--- src/HabboHotel/GameClient/GameClient.manager.ts
+++ src/HabboHotel/GameClient/GameClient.manager.ts
@@ -2,19 +2,20 @@ import * as ws from "ws";
 import * as net from "net";
 import { Injectable } from '@nestjs/common';
 import { GameClientDefs } from './GameClient.defs';
+import { OutgoingPacket } from "src/Message/Outgoing/Outgoing.packet";
 
 @Injectable()
 export class GameClientManager {
-    private users: Map<number, GameClientDefs>;
+    private clients: Map<number, GameClientDefs>;
     private lastConnectionId: number = 1;
 
     constructor() {
-        this.users = new Map<number, GameClientDefs>();
+        this.clients = new Map<number, GameClientDefs>();
     }
 
     public addUser(id: number, socket: ws | net.Socket): boolean {
         var gc: GameClientDefs = new GameClientDefs(socket);
-        return this.users.set(id, gc) == null;
+        return this.clients.set(id, gc) == null;
     }
 
     public get LastConnectionId(): number {
@@ -32,11 +33,17 @@ export class GameClientManager {
         }
     }
 
+    public sendToAll(message: OutgoingPacket | Array<OutgoingPacket>): void {
+        for (var client of this.clients.values()) {
+            client.send(message);
+        }
+    }
+
     public get userCounter(): number {
-        return this.users.size;
+        return this.clients.size;
     }
 
     public getUser(connectionId: number): GameClientDefs {
-        return this.users.get(connectionId);
+        return this.clients.get(connectionId);
     }
 }
--- src/Networking/RCON/RCON.manager.ts
+++ src/Networking/RCON/RCON.manager.ts
@@ -1,5 +1,6 @@
 import * as http from "http";
 import * as net from "net";
+import * as url from "url";
 import { Injectable, Logger } from '@nestjs/common';
 import { RCONMessageInterface } from "src/Message/RCON/RCONMessage.interface";
 import { ConfigurationManager } from "src/Core/Configuration/Configuration.manager";
@@ -9,23 +10,20 @@ export class RCONManager {
     private readonly logger = new Logger(RCONManager.name);
     private server: http.Server;
     private rconMessage: Map<string, RCONMessageInterface>;
-    private ipWhitelist: Array<string>;
 
     constructor(
         private readonly configurationManager: ConfigurationManager
     ) {
         this.rconMessage = new Map<string, RCONMessageInterface>();
-        this.ipWhitelist = new Array<string>();
 
         this.setMessage();
-        this.setIpWhitelist();
         this.init();
     }
 
     private init(): void {
         this.server = http.createServer();
         this.server.on('connection', (socket: net.Socket) => {
-            if (!this.ipWhitelist.includes(socket.remoteAddress)) {
+            if (!this.configurationManager.getString('rcon.ip_whitelist').split(",")[socket.remoteAddress] == null) {
                 socket.end();
                 return;
             }
@@ -45,18 +43,16 @@ export class RCONManager {
         
     }
 
-    private setIpWhitelist(): void {
-        for (var ip of this.configurationManager.getString('rcon.ip_whitelist').split(";")) {
-            this.ipWhitelist.push(ip);
-        }
-    }
-
     public handleRCONMessage(message: any): void {
         if (this.rconMessage.has(message.key)) {
             if (this.configurationManager.getBoolean('rcon.message_log')) {
                 this.logger.log("RCON message executed: " + message.key);
             }
             this.rconMessage.get(message.key).read(message.data);
+        } else {
+            if (this.configurationManager.getBoolean('rcon.message_log')) {
+                this.logger.log("RCON message unrecognized: " + message.key);
+            }
         }
     }
 }
